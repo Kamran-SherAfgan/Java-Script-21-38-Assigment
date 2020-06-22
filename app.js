cb61a22782335d08b2d43a334a6a8945bd913cad
Kamran Sher Afgan <64993556+Kamran-SherAfgan@users.noreply.github.com>
2020-06-22 10:01:50 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,180 @@
+// Chapter # 21 Task 1
+
+// var firstName = prompt("Enter first name");
+// var lastName = prompt("Enter last name");
+// var titleName = (firstName + " " + lastName);
+// alert(titleName);
+
+// Task # 2
+
+// var phoneModel = prompt("Enter phone model");
+// var length = phoneModel.length
+// document.write("My favorite phone is: " + phoneModel + "<br>")
+// document.write("length of string is: " + length )
+
+// Task # 3
+
+// var word = "Pakistani"
+// var index = word.indexOf("n");
+// document.write("String: " + word + "<br>")
+// document.write("Index of 'n' is " + index);
+
+// Task # 4
+
+// var word = "hello world"
+// var index = word.lastIndexOf("l");
+// document.write("String: " + word + "<br>")
+// document.write("Last index of 'l': " + index);
+
+// Task # 5
+
+
+// var word = "Pakistani"
+// var index = word.charAt(3);
+// document.write("String: " + word + "<br>")
+// document.write("Character at  index 3: " + index);
+
+// Task # 6
+
+// var firstName = prompt("Enter first name");
+// var lastName = prompt("Enter last name");
+// var titleName = firstName.concat(lastName);
+// alert(titleName);
+
+// Task # 7
+
+// var city = "Hyderabad";
+// var islam = city.replace("Hyder" , "Islam");
+// document.write("City: " + city + "<br>")
+// document.write("After replacment: " + islam);
+ 
+
+// Task # 8
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var replace = message.replace(/and/g , "&");
+// document.write(replace)
+
+// Task # 9
+
+// var number = 472;
+// var converted = number.toString();
+// document.write("Value: " + converted + "<br>");
+// document.write("Type: string"  + "<br>");
+
+
+// var string = "472"
+// var converted2 = Number(string);
+// document.write("Value: " + converted2 + "<br>")
+// document.write("Type: number" )
+
+
+// Task # 10
+
+// var word = prompt("Please enter someyhing");
+// var cahnged = word.toUpperCase();
+// alert(cahnged);
+
+// Task # 11
+
+// var userName = prompt("Enter user name");
+// var invalid = "!";
+// if ( userName === invalid ) {
+//     alert("Invalid username");
+// }
+// else|{
+//     alert('Valid username');
+// }
+
+// Task # 12
+
+// var A = ['cake' , 'apple pie' , 'cookie' , 'chips' , 'patties'];
+// var search = prompt('Welcome to ABC bakery. What do you want to order?')
+// if ( search === A ) {
+//     alert('This item is available!');
+// }  else {
+//     alert('This item is not available!');
+// }
+
+// Task # 13
+
+// var university = 'University of Karachi';
+// var newUni = university.split();
+// for(var i = 0; i <= newUni; i+19) {
+//     if ( i === newUni) {
+//         document.write(i + '<br>');
+//     }
+// }
+
+
+// Task # 14
+
+// var country = 'Pakistan';
+// var ask = prompt('Enter country');
+// if ( ask === country) {
+//     document.write('n');
+
+// Task # 15
+
+// var integer = +prompt('Enter enteger');
+// document.write('number; ' + integer + '<br>')
+// document.write('round off value: ' + Math.round(integer) + '<br>');
+// document.write('floor value: ' + Math.floor(integer) + '<br>');
+// document.write('ceil value: ' + Math.ceil(integer))
+
+
+// Task # 16
+
+// var integer = +prompt('Enter enteger');
+// document.write('number; ' + integer + '<br>')
+// document.write('round off value: ' + Math.round(integer) + '<br>');
+// document.write('floor value: ' + Math.floor(integer) + '<br>');
+// document.write('ceil value: ' + Math.ceil(integer))
+
+
+// Task # 17
+
+// var ask = +prompt('Enter number');
+// var absolute = Math.abs(ask);
+
+// document.write('The absolute value of ' + ask + ' is '  + absolute);
+
+
+// Task # 18
+
+// var number = Math.random();
+// if ( number < 1) {
+//     document.write('random coin value: Heads')
+// } else {
+//     document.write( 'random coin value: Tails' )
+// }
+
+
+// Task # 19
+
+// var ask = +prompt('Enter number between 1 to 10');
+
+// var bigDecimal = Math.random();
+// var improvedNum = (bigDecimal * 10); 
+// var numberOfStars = Math.floor(improvedNum);
+// if ( ask = numberOfStars ) {
+//     document.write('Congrate!');
+
+// } else {
+//     document.write('Try again');
+
+// }
+
+
+// Task # 20
+
+// var currentDate = new Date;
+// document.write(currentDate);
+
+
+// Task # 21
+
+// var date = new Date;
+// var month = date.getMonth();
+// document.write(month);
+
